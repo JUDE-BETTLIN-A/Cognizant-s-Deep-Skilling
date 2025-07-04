3f40093404d0d8e0c5515e28878b6b91184c766e
JUDE-BETTLIN-A <judebettlin@gmail.com>
2025-07-04 22:21:35 +0530

    Week 3

---
+++ Week 3 Spring Core, Maven, Spring Data JPA/Spring Data JPA with Hibernate/3.Creating Repositories/employee-management/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (id, name) VALUES (1, 'HR'), (2, 'Engineering');
+
+INSERT INTO employee (id, name, email, department_id) VALUES
+(1, 'Alice', 'alice@example.com', 1),
+(2, 'Bob', 'bob@example.com', 2);
